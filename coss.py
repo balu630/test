3eb18665ae9972920dfade256e5caec8f0722738
Ubuntu <ubuntu@ip-172-31-44-178.eu-north-1.compute.internal>
2026-06-16 16:21:28 +0000

    check

--- coss.py
+++ coss.py
@@ -4,3 +4,5 @@ Theme-SMART EDUCATION
 PS Category- Software/Hardware
 Team ID-94
 Team Name –SAVYASACHI
+
+Lack of Guidance – Many students who want to study abroad are confused about college selection, applications, SOP/LOR writing, and visa process.
