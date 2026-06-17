4e9110535e219c729473a79068c91e810f70dd19
Madha Srishanthgoud <balumadha0@gmail.com>
2026-06-17 22:44:26 +0530

    wishes message

--- coss.py
+++ coss.py
@@ -5,8 +5,7 @@ PS Category- Software/Hardware
 Team ID-94
 Team Name –SAVYASACHI
 
-Lack of Guidance – Many students who want to study abroad are confused about college selection, applications, SOP/LOR writing, and visa process.
-ON
+
 DATA REUSE
 FOR
 ALTERNATE ASSESSMENT
@@ -14,6 +13,5 @@ IN
 AUTOMAT THEORY AND COMPILER DESIGN
 BY
 M.YASHWANTH    23VE1A05G3
-
-
 ACADEMIC YEAR : 2025-20266
+welcome to devops
