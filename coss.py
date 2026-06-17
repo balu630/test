3496076b5ed7762827ea428d4fa7678290a21c54
Ubuntu <ubuntu@ip-172-31-44-178.eu-north-1.compute.internal>
2026-06-17 15:43:22 +0000

    testing purpose

--- coss.py
+++ coss.py
@@ -6,3 +6,14 @@ Team ID-94
 Team Name –SAVYASACHI
 
 Lack of Guidance – Many students who want to study abroad are confused about college selection, applications, SOP/LOR writing, and visa process.
+ON
+DATA REUSE
+FOR
+ALTERNATE ASSESSMENT
+IN
+AUTOMAT THEORY AND COMPILER DESIGN
+BY
+M.YASHWANTH    23VE1A05G3
+
+
+ACADEMIC YEAR : 2025-20266
